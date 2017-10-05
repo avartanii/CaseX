f36a09c8ea0df93b730ada12e1ad2fe66ac874cc
Eileen Choe <echoe@lion.lmu.edu>
2017-10-04 19:29:26 -0700

    file upload with ajax + cors

--- scripts/mongo-check.sh
+++ scripts/mongo-check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # https://gist.githubusercontent.com/m-szk/1306735/raw/6d3df5418f4161b4d858200ac962f9eab15ab761/mongod.sh
-echo `ps -ef | grep mongod | grep -v grep | wc -l | tr -d ' '`
+# echo `ps -ef | grep mongod | grep -v grep | wc -l | tr -d ' '`
 grep_mongo=`ps -ef | grep mongod | grep -v grep | wc -l | tr -d ' '`
-echo $grep_mongo
+# echo $grep_mongo
 if [ $grep_mongo -gt 0 ]
   then echo "MongoDB is already running."
   else echo "Start MongoDB."
-	`mongod --fork --logpath /var/log/mongodb.log`
-  RETVAL=$?
+	`mongod`
+  # RETVAL=$?
 fi
