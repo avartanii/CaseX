34790aba4aeec1e5ad7869880e51b7b97370f5f8
Andrea Carver <ansonadams27@gmail.com>
2017-10-27 12:09:06 -0700

    Refactored form submission, linked inputs

--- web-client/public/js/submitInputForm.js
+++ web-client/public/js/submitInputForm.js
@@ -2,16 +2,81 @@ window.InputController = (() => {
   return {
     init: () => {
 
-      const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+      // Link UI inputs
+      var drNumInput = $("#drNumInput");
+      var masterDrNumInput = $("#masterDrNumInput");
+      var divisionInput = $("#divisionInput");
+      var bureauInput = $("#bureauInput");
+      var notesInput = $("#notesInput");
+      var dateOccuredInput = $("#dateOccuredInput");
+      var dateReportedInput = $("#dateReportedInput");
+      var reportingDistrictInput = $("#reportingDistrictInput");
+      var caseStatusInput = $("#caseStatusInput");
+      var caseStatusDateInput = $("#caseStatusDateInput");
+      var solvabilityFactorInput = $("#solvabilityFactorInput");
+      var weaponInput_handgun = $("#weaponInput_handgun");
+      var weaponInput_rifle = $("#weaponInput_rifle");
+      var weaponInput_bluntForce = $("#weaponInput_bluntForce");
+      var weaponInput_bodilyForce = $("#weaponInput_bodilyForce");
+      var weaponInput_knife = $("#weaponInput_knife");
+      var weaponInput_unknown = $("#weaponInput_unknown");
+      var motiveInput_robbery = $("#motiveInput_robbery");
+      var motiveInput_burglary = $("#motiveInput_burglary");
+      var motiveInput_gang = $("#motiveInput_gang");
+      var motiveInput_narcotics = $("#motiveInput_narcotics");
+      var motiveInput_domesticViolence = $("#motiveInput_domesticViolence");
+      var motiveInput_dispute = $("#motiveInput_dispute");
+      var motiveInput_accidental = $("#motiveInput_accidental");
+      var motiveInput_selfDefense = $("#motiveInput_selfDefense");
+      var motiveInput_unknown = $("#motiveInput_unknown");
+      var streetNumberInput = $("#streetNumberInput");
+      var streetNameInput = $("#streetNameInput");
+      var cityInput = $("#cityInput");
+      var zipCodeInput = $("#zipCodeInput");
+      var victNameInput = $("#victNameInput");
+      var victSexInput = $("#victSexInput");
+      var victSupervisedReleaseStatusInput = $("#victSupervisedReleaseStatusInput");
+      var victDescInput = $("#victDescInput");
+      var victAgeInput = $("#victAgeInput");
+      var victIdInput = $("#victIdInput");
+      var suspNameInput = $("#suspNameInput");
+      var suspSexInput = $("#suspSexInput");
+      var suspSupervisedReleaseStatusInput = $("#suspSupervisedReleaseStatusInput");
+      var suspDescInput = $("#suspDescInput");
+      var suspAgeInput = $("#suspAgeInput");
+      var suspIdInput = $("#suspIdInput");
+      var newOrExistingVictimInput = $("#newOrExistingVictimInput");
+      var newOrExistingSuspectInput = $("#newOrExistingSuspectInput");
 
+      const formToJSON = elements => [].reduce.call(elements, (data, element) => {
         data[element.name] = element.value;
         return data;
-
       }, {});
 
-      const handleFormSubmit = event => {
+      function attemptFormSubmission() {
+        if (checkFormValidityAndAnnotate()) {
+          submitCaseForm(getCaseDataAsJSON());
+        }
+      }
 
-        const data = formToJSON(form.elements);
+      function checkFormValidityAndAnnotate() {
+        var isValid = true;
+        if () {
+          // TODO: Highlight UI
+          isValid = false;
+        } else if () {
+          // TODO: Highlight UI
+          isValid = false;
+        }
+        else {
+          return isValid;
+        }
+      }
+
+      function submitCaseForm(data) {
+
+        const caseForm = document.getElementsByClassName('caseForm')[0]
+        const caseData = formToJSON(caseForm.elements);
 
         // Trying Ajax:
         $.ajax({
@@ -51,13 +116,28 @@ window.InputController = (() => {
 
       }
 
-      const form = document.getElementsByClassName('caseForm')[0]
-      form.addEventListener('submit', handleFormSubmit);
+      function submitVictimForm(data) {
+        // TODO
+      }
+
+      function submitSuspectForm(data) {
+        // TODO
+      }
+
+      function getCaseDataAsJSON() {
+        // TODO
+      }
+
+      // UI functionality
 
       $('#button-submit-forms').on('click', function(){
-        handleFormSubmit();
+        attemptFormSubmission();
       })
-      
+
+      // TODO: hide/show UI
+
+
+
     }
 
   };
