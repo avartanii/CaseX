39eaa7cbc7766a886ce96c729fcaa5d3d7ec5d3f
Andrea Carver <ansonadams27@gmail.com>
2017-10-27 12:41:32 -0700

    Master form only shows relevant inputs

--- web-client/public/js/submitInputForm.js
+++ web-client/public/js/submitInputForm.js
@@ -3,50 +3,54 @@ window.InputController = (() => {
     init: () => {
 
       // Link UI inputs
-      var drNumInput = $("#drNumInput");
-      var masterDrNumInput = $("#masterDrNumInput");
-      var divisionInput = $("#divisionInput");
-      var bureauInput = $("#bureauInput");
-      var notesInput = $("#notesInput");
-      var dateOccuredInput = $("#dateOccuredInput");
-      var dateReportedInput = $("#dateReportedInput");
-      var reportingDistrictInput = $("#reportingDistrictInput");
-      var caseStatusInput = $("#caseStatusInput");
-      var caseStatusDateInput = $("#caseStatusDateInput");
-      var solvabilityFactorInput = $("#solvabilityFactorInput");
-      var weaponInput_handgun = $("#weaponInput_handgun");
-      var weaponInput_rifle = $("#weaponInput_rifle");
-      var weaponInput_bluntForce = $("#weaponInput_bluntForce");
-      var weaponInput_bodilyForce = $("#weaponInput_bodilyForce");
-      var weaponInput_knife = $("#weaponInput_knife");
-      var weaponInput_unknown = $("#weaponInput_unknown");
-      var motiveInput_robbery = $("#motiveInput_robbery");
-      var motiveInput_burglary = $("#motiveInput_burglary");
-      var motiveInput_gang = $("#motiveInput_gang");
-      var motiveInput_narcotics = $("#motiveInput_narcotics");
-      var motiveInput_domesticViolence = $("#motiveInput_domesticViolence");
-      var motiveInput_dispute = $("#motiveInput_dispute");
-      var motiveInput_accidental = $("#motiveInput_accidental");
-      var motiveInput_selfDefense = $("#motiveInput_selfDefense");
-      var motiveInput_unknown = $("#motiveInput_unknown");
-      var streetNumberInput = $("#streetNumberInput");
-      var streetNameInput = $("#streetNameInput");
-      var cityInput = $("#cityInput");
-      var zipCodeInput = $("#zipCodeInput");
-      var victNameInput = $("#victNameInput");
-      var victSexInput = $("#victSexInput");
-      var victSupervisedReleaseStatusInput = $("#victSupervisedReleaseStatusInput");
-      var victDescInput = $("#victDescInput");
-      var victAgeInput = $("#victAgeInput");
-      var victIdInput = $("#victIdInput");
-      var suspNameInput = $("#suspNameInput");
-      var suspSexInput = $("#suspSexInput");
-      var suspSupervisedReleaseStatusInput = $("#suspSupervisedReleaseStatusInput");
-      var suspDescInput = $("#suspDescInput");
-      var suspAgeInput = $("#suspAgeInput");
-      var suspIdInput = $("#suspIdInput");
-      var newOrExistingVictimInput = $("#newOrExistingVictimInput");
-      var newOrExistingSuspectInput = $("#newOrExistingSuspectInput");
+      var drNumInput = $('#drNumInput');
+      var masterDrNumInput = $('#masterDrNumInput');
+      var divisionInput = $('#divisionInput');
+      var bureauInput = $('#bureauInput');
+      var notesInput = $('#notesInput');
+      var dateOccuredInput = $('#dateOccuredInput');
+      var dateReportedInput = $('#dateReportedInput');
+      var reportingDistrictInput = $('#reportingDistrictInput');
+      var caseStatusInput = $('#caseStatusInput');
+      var caseStatusDateInput = $('#caseStatusDateInput');
+      var solvabilityFactorInput = $('#solvabilityFactorInput');
+      var weaponInput_handgun = $('#weaponInput_handgun');
+      var weaponInput_rifle = $('#weaponInput_rifle');
+      var weaponInput_bluntForce = $('#weaponInput_bluntForce');
+      var weaponInput_bodilyForce = $('#weaponInput_bodilyForce');
+      var weaponInput_knife = $('#weaponInput_knife');
+      var weaponInput_unknown = $('#weaponInput_unknown');
+      var motiveInput_robbery = $('#motiveInput_robbery');
+      var motiveInput_burglary = $('#motiveInput_burglary');
+      var motiveInput_gang = $('#motiveInput_gang');
+      var motiveInput_narcotics = $('#motiveInput_narcotics');
+      var motiveInput_domesticViolence = $('#motiveInput_domesticViolence');
+      var motiveInput_dispute = $('#motiveInput_dispute');
+      var motiveInput_accidental = $('#motiveInput_accidental');
+      var motiveInput_selfDefense = $('#motiveInput_selfDefense');
+      var motiveInput_unknown = $('#motiveInput_unknown');
+      var streetNumberInput = $('#streetNumberInput');
+      var streetNameInput = $('#streetNameInput');
+      var cityInput = $('#cityInput');
+      var zipCodeInput = $('#zipCodeInput');
+      var victNameInput = $('#victNameInput');
+      var victSexInput = $('#victSexInput');
+      var victSupervisedReleaseStatusInput = $('#victSupervisedReleaseStatusInput');
+      var victDescInput = $('#victDescInput');
+      var victAgeInput = $('#victAgeInput');
+      var victIdInput = $('#victIdInput');
+      var suspNameInput = $('#suspNameInput');
+      var suspSexInput = $('#suspSexInput');
+      var suspSupervisedReleaseStatusInput = $('#suspSupervisedReleaseStatusInput');
+      var suspDescInput = $('#suspDescInput');
+      var suspAgeInput = $('#suspAgeInput');
+      var suspIdInput = $('#suspIdInput');
+      var newOrExistingVictimInput = $('#newOrExistingVictimInput');
+      var newOrExistingSuspectInput = $('#newOrExistingSuspectInput');
+      var newVictimForm = $('#newVictimForm');
+      var existingVictimForm = $('#existingVictimForm');
+      var newSuspectForm = $('#newSuspectForm');
+      var existingSuspectForm = $('#existingSuspectForm');
 
       const formToJSON = elements => [].reduce.call(elements, (data, element) => {
         data[element.name] = element.value;
@@ -61,10 +65,10 @@ window.InputController = (() => {
 
       function checkFormValidityAndAnnotate() {
         var isValid = true;
-        if () {
+        if (true) {
           // TODO: Highlight UI
           isValid = false;
-        } else if () {
+        } else if (true) {
           // TODO: Highlight UI
           isValid = false;
         }
@@ -81,11 +85,11 @@ window.InputController = (() => {
         // Trying Ajax:
         $.ajax({
           type: 'POST',
-          url: "http://localhost:3000/case",
-          data: JSON.stringify(data, null, "  "),
-          contentType: "application/json",
-          dataType: "json",
-          accept: "application/json",
+          url: 'http://localhost:3000/case',
+          data: JSON.stringify(data, null, '  '),
+          contentType: 'application/json',
+          dataType: 'json',
+          accept: 'application/json',
           processData: false,
           contentType: false,
           crossDomain: true, // Added
@@ -93,8 +97,8 @@ window.InputController = (() => {
             alert(data);
           },
           complete: function () {
-            console.log("DEBUG: Found data:")
-            console.log(JSON.stringify(data, null, "  "));;
+            console.log('DEBUG: Found data:')
+            console.log(JSON.stringify(data, null, '  '));;
           }
         });
 
@@ -130,13 +134,49 @@ window.InputController = (() => {
 
       // UI functionality
 
-      $('#button-submit-forms').on('click', function(){
+      $('#button-submit-forms').on('click', function() {
         attemptFormSubmission();
-      })
+      });
 
-      // TODO: hide/show UI
+      // Hide/show UI
 
+      function updateVictimInputsVisibility() {
+        var val = newOrExistingVictimInput.val();
+        if (val == 'default') {
+          newVictimForm.hide();
+          existingVictimForm.hide();
+        } else if (val == 'new') {
+          newVictimForm.show();
+          existingVictimForm.hide();
+        } else if (val == 'old') {
+          newVictimForm.hide();
+          existingVictimForm.show();
+        }
+      }
+      updateVictimInputsVisibility();
 
+      function updateSuspectInputsVisibility() {
+        var val = newOrExistingSuspectInput.val();
+        if (val == 'default') {
+          newSuspectForm.hide();
+          existingSuspectForm.hide();
+        } else if (val == 'new') {
+          newSuspectForm.show();
+          existingSuspectForm.hide();
+        } else if (val == 'old') {
+          newSuspectForm.hide();
+          existingSuspectForm.show();
+        }
+      }
+      updateSuspectInputsVisibility();
+
+      newOrExistingVictimInput.change(function() {
+        updateVictimInputsVisibility();
+      });
+
+      newOrExistingSuspectInput.change(function() {
+        updateSuspectInputsVisibility();
+      });
 
     }
 
