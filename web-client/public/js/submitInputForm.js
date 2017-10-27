48a8c4db0d36d3eb066e78dbf87f84042628536e
Andrea Carver <ansonadams27@gmail.com>
2017-10-27 11:20:27 -0700

    Moved form js to window

--- web-client/public/js/submitInputForm.js
+++ web-client/public/js/submitInputForm.js
@@ -1,58 +1,65 @@
-(() => {
+window.InputController = (() => {
+  return {
+    init: () => {
 
-  const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+      const formToJSON = elements => [].reduce.call(elements, (data, element) => {
 
-    data[element.name] = element.value;
-    return data;
+        data[element.name] = element.value;
+        return data;
 
-  }, {});
+      }, {});
 
-  const handleFormSubmit = event => {
+      const handleFormSubmit = event => {
 
-    const data = formToJSON(form.elements);
+        const data = formToJSON(form.elements);
 
-    // Trying Ajax:
-    $.ajax({
-      type: 'POST',
-      url: "http://localhost:3000/case",
-      data: JSON.stringify(data, null, "  "),
-      contentType: "application/json",
-      dataType: "json",
-      accept: "application/json",
-      processData: false,
-      contentType: false,
-      crossDomain: true, // Added
-      success: function(data){
-        alert(data);
-      },
-      complete: function () {
-        console.log("DEBUG: Found data:")
-        console.log(JSON.stringify(data, null, "  "));;
+        // Trying Ajax:
+        $.ajax({
+          type: 'POST',
+          url: "http://localhost:3000/case",
+          data: JSON.stringify(data, null, "  "),
+          contentType: "application/json",
+          dataType: "json",
+          accept: "application/json",
+          processData: false,
+          contentType: false,
+          crossDomain: true, // Added
+          success: function(data){
+            alert(data);
+          },
+          complete: function () {
+            console.log("DEBUG: Found data:")
+            console.log(JSON.stringify(data, null, "  "));;
+          }
+        });
+
+        // Trying request:
+        // request('http://localhost:3000/case', { json: data }, (err, res, body) => {
+        //   if (err) { return console.log(err); }
+        //   console.log(body.url);
+        //   console.log(body.explanation);
+        // });
+
+        // Trying Axios:
+        // axios.post('http://localhost:3000/case', {firstName: 'joe'})
+        //   .then(function(response) {
+        //     console.log(response);
+        //   })
+        //   .catch(function(error) {
+        //     console.log(error);
+        //   });
+
       }
-    });
 
-    // Trying request:
-    // request('http://localhost:3000/case', { json: data }, (err, res, body) => {
-    //   if (err) { return console.log(err); }
-    //   console.log(body.url);
-    //   console.log(body.explanation);
-    // });
+      const form = document.getElementsByClassName('caseForm')[0]
+      form.addEventListener('submit', handleFormSubmit);
 
-    // Trying Axios:
-    // axios.post('http://localhost:3000/case', {firstName: 'joe'})
-    //   .then(function(response) {
-    //     console.log(response);
-    //   })
-    //   .catch(function(error) {
-    //     console.log(error);
-    //   });
+      $('#button-submit-forms').on('click', function(){
+        handleFormSubmit();
+      })
+      
+    }
 
-  }
+  };
 
-  const form = document.getElementsByClassName('caseForm')[0]
-  form.addEventListener('submit', handleFormSubmit);
-
-  $('#button-submit-forms').on('click', function(){
-    handleFormSubmit();
-  })
 })();
