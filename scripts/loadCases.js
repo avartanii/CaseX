42afd6d4b663c3f7b53fdd5cc7da0925b875331e
Eileen Choe <echoe@lion.lmu.edu>
2017-10-30 11:31:49 -0700

    complete db setup scripts

---
+++ scripts/loadCases.js
@@ -0,0 +1,136 @@
+var request = require('request');
+var rp = require('request-promise');
+var baseUrl = 'http://localhost:3000/';
+var victimIDs = [];
+var suspectIDs = [];
+var userIDs = [];
+
+var getVictims = function () {
+  return rp({
+    method: 'GET',
+    uri: baseUrl + 'victims',
+    json: true, // Automatically parses the JSON string in the response
+  }).then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      victimIDs.push(data[i]['_id']);
+    }
+  });
+};
+
+var getSuspects = function () {
+  return rp({
+    method: 'GET',
+    uri: baseUrl + 'suspects',
+    json: true, // Automatically parses the JSON string in the response
+  }).then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      suspectIDs.push(data[i]['_id']);
+    }
+  });
+};
+
+var getUsers = function () {
+  return rp({
+    method: 'GET',
+    uri: baseUrl + 'users',
+    json: true, // Automatically parses the JSON string in the response
+  }).then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      userIDs.push(data[i]['_id']);
+    }
+  });
+};
+
+var drNumCount = 0;
+
+var randomData = function (data) {
+  var randomInt = Math.floor(Math.random() * (data.length - 1));
+  return data[randomInt];
+};
+
+var randomArray = function (data) {
+  var randomData = [];
+  var randomLength = Math.floor(Math.random() * data.length) + 1;
+  for (var i = 0; i < randomLength; i++) {
+    var randomIndex = Math.floor(Math.random() * (data.length - 1));
+    randomData.push(data[randomIndex]);
+  }
+  return randomData;
+};
+
+var randomDate = function () {
+  return new Date(+(new Date()) - Math.floor(Math.random() * 10000000000));
+};
+
+var randomCase = function () {
+  var division = ['Southwest', 'Southeast', '77th Street', 'Harbor'];
+  var bureau = ['OSB', 'OCB', 'OWB', 'OVB'];
+  var reportingDistrict = ['a', 'b', 'c'];
+  var caseStatus = ['Open', 'Closed'];
+  var solvabilityFactor = ['Easy', 'Medium', 'Hard'];
+  var weaponUsed = ['handgun', 'blunt force', 'unknown', 'rifle', 'bodily force', 'knife'];
+  var motive = ['gang', 'unknown', 'robbery', 'narcotics', 'domestic violence',
+    'dispute', 'accidental', 'self defense', 'burglary'];
+  var address = [
+    {
+      streetNumber: 30,
+      streetName: 'Baker Street',
+      city: 'Los Angeles',
+      zipCode: 90234
+    },
+    {
+      streetNumber: 4244,
+      streetName: 'Main Street',
+      city: 'Los Angeles',
+      zipCode: 90222
+    },
+    {
+      streetNumber: 2343,
+      streetName: '5th Avenue',
+      city: 'Los Angeles',
+      zipCode: 90253
+    },
+  ];
+  var caseForm = {
+    drNumber: drNumCount,
+    masterDrNumber: drNumCount,
+    division: randomData(division),
+    bureau: randomData(bureau),
+    dateOccured: randomDate(),
+    dateReported: randomDate(),
+    reportingDistrict: randomData(reportingDistrict),
+    caseStatus: randomData(caseStatus),
+    solvabilityFactor: randomData(solvabilityFactor),
+    weaponUsed: randomArray(weaponUsed),
+    motive: randomArray(motive),
+    lastModifiedBy: randomData(userIDs),
+    victim: randomData(userIDs),
+    address: randomData(address),
+    suspects: randomArray(suspectIDs)
+  };
+  drNumCount++;
+  return caseForm;
+};
+
+Promise.all([getVictims(), getSuspects(), getUsers()])
+  .then(function () {
+    for (var i = 0; i < 10; i ++) {
+      var caseForm = randomCase();
+      var caseFormData = JSON.stringify(caseForm);
+      var contentLength = caseFormData.length;
+      request({
+        headers: {
+          'Content-Length': contentLength,
+          'Content-Type': 'application/json'
+        },
+        uri: baseUrl + 'case',
+        body: caseFormData,
+        method: 'POST'
+      }, function (err, res, body) {
+        if (err) {
+          console.log('err: ', err);
+        }
+        console.log('body: ' + body + ', res: ' + res.statusCode);
+      });
+    }
+  });
--- scripts/loadData.js
+++ scripts/loadData.js
@@ -1,47 +1,122 @@
-var querystring = require('querystring');
 var request = require('request');
 var baseUrl = 'http://localhost:3000/';
 
-var caseData = [
-  '123314234234|456|Southwest',
-  '23412443242|234|Southwest'
-];
-
-var form = {
-  drNumber: '120873',
-  masterDrNumber: '123',
-  division: 'Southwest',
-  bureau: 'OSB',
-  notes: 'note',
-  dateOccured: '2008-10-09',
-  dateReported: '2000-10-30',
-  reportingDistrict: 'a',
-  caseStatus: 'Open',
-  caseStatusDate: '',
-  solvabilityFactor: 'Easy',
-  weaponUsed: 'handgun',
-  motive: 'a',
-  lastModifiedDate: '2009-3-29',
-  lastModifiedBy: '',
-  victim: '59ea608274225c44e088efcc',
-  address: '',
-  suspects: ['a', 'b', 'c']
+var stringToBoolean = function (s) {
+  if (s.toLowerCase() === 'true') {
+    return true;
+  }
+  return false;
 };
 
-var formData = querystring.stringify(form);
-var contentLength = formData.length;
+var victimData = [
+  'Jane|Victim|Doe|Female|Brown Hair, Brown Eyes|23',
+  'John|Victim|Doe|Male|Red Hair, Blue Eyes|25'
+];
 
-request({
-  headers: {
-    'Content-Length': contentLength,
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  uri: 'http://localhost:3000/case',
-  body: formData,
-  method: 'POST'
-}, function (err, res, body) {
-  if (err) {
-    console.log('err: ', err);
-  }
-  console.log(body);
+victimData.forEach(function (s) {
+  var victim = s.split('|');
+  var data = {
+    victName: {
+      first: victim[0],
+      middle: victim[1],
+      last: victim[2]
+    },
+    victSex: victim[3],
+    victDesc: victim[4],
+    victAge: +victim[5]
+  };
+  var victimFormData = JSON.stringify(data);
+  var contentLength = victimFormData.length;
+
+  request({
+    headers: {
+      'Content-Length': contentLength,
+      'Content-Type': 'application/json'
+    },
+    uri: baseUrl + 'victim',
+    body: victimFormData,
+    method: 'POST'
+  }, function (err, res, body) {
+    if (err) {
+      console.log('err: ', err);
+    }
+    console.log('body: ' + body + ', res: ' + res.statusCode);
+  });
+});
+
+
+var suspectData = [
+  'Adam|Suspect|Johnson|Male|29|False|parole',
+  'Stacy|Suspect|Greene|Female|23|False|parole'
+];
+
+suspectData.forEach(function (s) {
+  var user = s.split('|');
+  var data = {
+    suspName: {
+      first: user[0],
+      middle: user[1],
+      last: user[2]
+    },
+    suspSex: user[3],
+    suspAge: +user[4],
+    juvenileTriedAsAdult: stringToBoolean(user[5]),
+    supervisedReleaseStatus: user[6]
+  };
+  var suspectFormData = JSON.stringify(data);
+  var contentLength = suspectFormData.length;
+
+  request({
+    headers: {
+      'Content-Length': contentLength,
+      'Content-Type': 'application/json'
+    },
+    uri: baseUrl + 'suspect',
+    body: suspectFormData,
+    method: 'POST'
+  }, function (err, res, body) {
+    if (err) {
+      console.log('err: ', err);
+    }
+    console.log('body: ' + body + ', res: ' + res.statusCode);
+  });
+});
+
+
+
+var userData = [
+  'John|Smith|Doe|123456|Admin|jdoe@gmail.com|wordpass',
+  'Margaret|Rachel|Smith|234567|General|msmith@gmail.com|wordpass'
+];
+
+userData.forEach(function (s) {
+  var user = s.split('|');
+  var data = {
+    name: {
+      first: user[0],
+      middle: user[1],
+      last: user[2]
+    },
+    employeeID: +user[3],
+    permissionLevel: user[4],
+    email: user[5],
+    password: user[6]
+  };
+  var userFormData = JSON.stringify(data);
+  var contentLength = userFormData.length;
+
+  request({
+    headers: {
+      'Content-Length': contentLength,
+      'Content-Type': 'application/json'
+    },
+    uri: baseUrl + 'user',
+    body: userFormData,
+    method: 'POST'
+  }, function (err, res, body) {
+    if (err) {
+      console.log('err: ', err);
+    }
+    console.log('body: ' + body + ', res: ' + res.statusCode);
+  });
 });
