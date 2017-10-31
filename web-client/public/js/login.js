ba034bf35e115c31493e475072bedcb61f21ea98
Allen Vartanian <avartan4@lion.lmu.edu>
2017-10-31 09:46:54 -0700

    Working on typing return to login

--- web-client/public/js/login.js
+++ web-client/public/js/login.js
@@ -48,3 +48,68 @@ $(document).ready(function () {
       });
   });
 });
+
+
+//
+// 
+// // function loginUser(username, password) {
+// //     axios.get('http://localhost:3000/users')
+// //       .then(function(response) {
+// //         var data = response.data;
+// //         console.log(username, password, response);
+// //
+// //         for (var i = 0; i < data.length; i++) {
+// //
+// //         }
+// //         console.log('FAILURE');
+// //       })
+// //       .catch(function(error) {
+// //         console.log(error);
+// //       });
+// // }
+//
+// var login = function () {
+//   var email = $('#email').val();
+//   var password = $('#password').val();
+//
+//   axios.get('http://localhost:3000/users')
+//     .then(function (response) {
+//       var data = response.data;
+//       // console.log('DATA: ', data);
+//       for (var i = 0; i < data.length; i++) {
+//         if (email === data[i]['email']) {
+//           // console.log('IM HERE', email, data[i]['email'], data[i]['password']);
+//           $.post('http://localhost:3000/login', {password: password, hash: data[i]['password']})
+//             .done(function (err, serverRes) {
+//             // console.log('SERVER RES1: ', serverRes);
+//             // console.log('ERR: ', err);
+//               if (serverRes === 'success') {
+//               // console.log('SERVER RES: ', serverRes);
+//                 window.location.href = '/';
+//               }
+//             })
+//             .fail(function () {
+//               alert('Incorrect password');
+//             });
+//         } else if (i === data.length) {
+//           alert('Incorrect email');
+//         }
+//       }
+//     });
+// };
+//
+// var returnKey = function (event) {
+//   if (event.keyCode === 13) {
+//     $('#login').click(login);
+//   }
+// };
+//
+// $('#email').keyup(returnKey);
+//
+// $('#password').keyup(returnKey);
+//
+// $(document).ready(function () {
+//   var email;
+//   var password;
+//   $('#login').click();
+// });
