d42a4c19c8f2f3f75e0bdc6decbd3fae272b5f9d
Allen Vartanian <avartan4@lion.lmu.edu>
2017-10-31 11:16:31 -0700

    Login work
    + 'Return' to login
    + Cleaned up code (removed console.logs)

--- web-client/public/js/login.js
+++ web-client/public/js/login.js
@@ -1,40 +1,20 @@
 
-// function loginUser(username, password) {
-//     axios.get('http://localhost:3000/users')
-//       .then(function(response) {
-//         var data = response.data;
-//         console.log(username, password, response);
-//
-//         for (var i = 0; i < data.length; i++) {
-//
-//         }
-//         console.log('FAILURE');
-//       })
-//       .catch(function(error) {
-//         console.log(error);
-//       });
-// }
+var login = function () {
+  var email = $('#email').val();
+  var password = $('#password').val();
 
-$(document).ready(function () {
-  var email;
-  var password;
-  $('#login').click(function () {
-    email = $('#email').val();
-    password = $('#password').val();
-
+  if (email.length === 0) {
+    alert('Incorrect email');
+  } else {
     axios.get('http://localhost:3000/users')
       .then(function (response) {
         var data = response.data;
-        // console.log('DATA: ', data);
         for (var i = 0; i < data.length; i++) {
+          console.log('in for: ', i, data.length);
           if (email === data[i]['email']) {
-            // console.log('IM HERE', email, data[i]['email'], data[i]['password']);
             $.post('http://localhost:3000/login', {password: password, hash: data[i]['password']})
               .done(function (err, serverRes) {
-              // console.log('SERVER RES1: ', serverRes);
-              // console.log('ERR: ', err);
                 if (serverRes === 'success') {
-                // console.log('SERVER RES: ', serverRes);
                   window.location.href = '/';
                 }
               })
@@ -46,70 +26,21 @@ $(document).ready(function () {
           }
         }
       });
-  });
+  }
+
+};
+
+var returnKey = function (event) {
+  if (event.keyCode === 13) {
+    console.log('yo');
+    $('#login').click();
+  }
+};
+
+$('#email').keyup(returnKey);
+
+$('#password').keyup(returnKey);
+
+$(document).ready(function () {
+  $('#login').click(login);
 });
-
-
-//
-// 
-// // function loginUser(username, password) {
-// //     axios.get('http://localhost:3000/users')
-// //       .then(function(response) {
-// //         var data = response.data;
-// //         console.log(username, password, response);
-// //
-// //         for (var i = 0; i < data.length; i++) {
-// //
-// //         }
-// //         console.log('FAILURE');
-// //       })
-// //       .catch(function(error) {
-// //         console.log(error);
-// //       });
-// // }
-//
-// var login = function () {
-//   var email = $('#email').val();
-//   var password = $('#password').val();
-//
-//   axios.get('http://localhost:3000/users')
-//     .then(function (response) {
-//       var data = response.data;
-//       // console.log('DATA: ', data);
-//       for (var i = 0; i < data.length; i++) {
-//         if (email === data[i]['email']) {
-//           // console.log('IM HERE', email, data[i]['email'], data[i]['password']);
-//           $.post('http://localhost:3000/login', {password: password, hash: data[i]['password']})
-//             .done(function (err, serverRes) {
-//             // console.log('SERVER RES1: ', serverRes);
-//             // console.log('ERR: ', err);
-//               if (serverRes === 'success') {
-//               // console.log('SERVER RES: ', serverRes);
-//                 window.location.href = '/';
-//               }
-//             })
-//             .fail(function () {
-//               alert('Incorrect password');
-//             });
-//         } else if (i === data.length) {
-//           alert('Incorrect email');
-//         }
-//       }
-//     });
-// };
-//
-// var returnKey = function (event) {
-//   if (event.keyCode === 13) {
-//     $('#login').click(login);
-//   }
-// };
-//
-// $('#email').keyup(returnKey);
-//
-// $('#password').keyup(returnKey);
-//
-// $(document).ready(function () {
-//   var email;
-//   var password;
-//   $('#login').click();
-// });
