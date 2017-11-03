b80c9a923fa29f342f5549a86ac7759c22e5d92a
Andrea Carver <ansonadams27@gmail.com>
2017-11-03 13:59:52 -0700

    Finished delete user UI and functionality

--- web-client/public/js/page-admin.js
+++ web-client/public/js/page-admin.js
@@ -3,8 +3,9 @@ window.AdminController = (() => {
     init: () => {
 
       function updateDeleteUsersList() {
+        $('#deleteUsersInput').empty();
         $.get('http://localhost:3000/users', function(data) {
-          var option = '';
+          var option = '<option selected hidden value=\'default\'>Select User...</option>';
           for (user in data) {
             option += '<option value="' + data[user]['_id'] + '">' + data[user]['name']['first'] + ' ' + data[user]['name']['last'] + '</option>';
           }
@@ -19,10 +20,13 @@ window.AdminController = (() => {
 
       function attemptDeleteUser() {
         $('#deleteUsersInput').removeClass('is-invalid');
+        $('#deleteUsersSmall').removeClass('text-danger');
+        $('#deleteUsersSmall').removeClass('text-success');
         $('#deleteUsersSmall').text('');
         var val = $('#deleteUsersInput').val();
         if (val == 'default') {
           $('#deleteUsersInput').addClass('is-invalid');
+          $('#deleteUsersSmall').addClass('text-danger');
           $('#deleteUsersSmall').text('Must select a user to delete.');
         } else {
           deleteUser();
@@ -30,7 +34,19 @@ window.AdminController = (() => {
       }
 
       function deleteUser() {
-        console.log('Would have deleted user now.');
+        var val = $('#deleteUsersInput').val()
+        var name = $('#deleteUsersInput option:selected').text();
+        if (val != 'default') {
+          $.ajax({
+            url: 'http://localhost:3000/user/' + val,
+            type: 'DELETE',
+            success: function(result) {
+              $('#deleteUsersSmall').text('Deleted user ' + name + '.');
+              $('#deleteUsersSmall').addClass('text-success');
+              updateDeleteUsersList();
+            }
+          });
+        }
       }
 
     }
