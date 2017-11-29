09dbc3ac1c41853237470f076ccd51b3b1a540d9
Eileen Choe <echoe@lion.lmu.edu>
2017-11-28 23:53:11 -0800

    fix force logout

--- web-client/public/js/accessTokenHttpInterceptor.js
+++ web-client/public/js/accessTokenHttpInterceptor.js
@@ -1,15 +1,17 @@
-/* eslint-disable */
-
 // Sets up ajax success handler to store JWT in sessionStorage
 
-$( document ).ajaxSuccess(function( event, request, settings ) {
-  if (request.resonseJSON && request.responseJSON.hasOwnProperty('success') && !request.responseJSON.hasOwnProperty('validationError') && !request.responseJSON.success) {
-    alert("Session expired. Redirecting to login.");
-    window.location.href = '/login';
-  }
-
+$(document).ajaxSuccess(( event, request, settings ) => {
   if (settings.url === 'http://localhost:3000/authenticate' && request.responseJSON.success) {
-    var token = request.responseJSON.token;
+    const token = request.responseJSON.token;
     window.sessionStorage.setItem('userInfo-token', token);
+  }
+});
+
+$(document).ajaxError((event, request, settings) => {
+  if (request.responseJSON && request.responseJSON.hasOwnProperty('success')
+    && request.responseJSON.hasOwnProperty('validationError')
+    && !request.responseJSON.validationError && !request.responseJSON.success) {
+    alert('Session expired. Redirecting to login.');
+    window.location.href = '/login';
   }
 });
